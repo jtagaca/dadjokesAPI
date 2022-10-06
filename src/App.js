fc5513d80a164b3c8ce07c840314fbc9c11dc57c
James Tagaca <jtagaca@csub.edu>
2022-10-06 04:14:40 +0000

    Adds hello message to the app.js file

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component{
                 {/* hey hey */}
                 {/* ggkk */}
                 gg 
-                hey
+                hey hello my name is
                 {/* mymy */}
             </div>
         )
